5b3ea511bb3ea03cfead4da518f89541ab48244d
koval77 <wojtek78@yahoo.com>
2018-08-29 05:36:01 +0100

    randomnumbers

--- app/src/main/java/com/example/android/practiseset2/MainActivity.java
+++ app/src/main/java/com/example/android/practiseset2/MainActivity.java
@@ -4,10 +4,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import java.util.Random;
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
     int scoreTeamA=0;
     int scoreTeamB=0;
+//    Random r2=new Random();
+//    int r1=0;
+
+//    r1.nextInt(56);
+//    r1=(int)(Math.random()*3.0);
+//    operand1Value = (int) (Math.random()*19.0) + 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,42 +33,56 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Displays the given score for Team A.
      */
-    public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(score));
+    public void getQuestion(View view)
+    {    Random r1=new Random();
+        int pcn1=r1.nextInt(100);
+
+        Random r2=new Random();
+        int pcn2=r2.nextInt(100);
+        TextView number1View = (TextView) findViewById(R.id.number_1);
+        number1View.setText(String.valueOf(Integer.toBinaryString(pcn1)));
+        TextView number2View = (TextView) findViewById(R.id.number_2);
+        number2View.setText(String.valueOf(Integer.toBinaryString(pcn2)));
     }
-    public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
-        scoreView.setText(String.valueOf(score));
+    public void displayNumberOne(int score) {
+        TextView number1View = (TextView) findViewById(R.id.number_1);
+        number1View.setText(String.valueOf(score));
+        TextView number2View = (TextView) findViewById(R.id.number_2);
+        number2View.setText(String.valueOf(score));
+
+    }
+    public void displayNumberTwo(int score) {
+        TextView number2View = (TextView) findViewById(R.id.number_2);
+        number2View.setText(String.valueOf(score));
     }
     public void add3(View view){
         scoreTeamA=scoreTeamA+3;
-        displayForTeamA(scoreTeamA);
+        displayNumberOne(scoreTeamA);
     }
     public void add2(View view){
         scoreTeamA=scoreTeamA+2;
-        displayForTeamA(scoreTeamA);
+        displayNumberOne(scoreTeamA);
     }
     public void free(View view){
         scoreTeamA=scoreTeamA+1;
-        displayForTeamA((scoreTeamA));
+        displayNumberOne((scoreTeamA));
     }
     public void add3B(View view){
         scoreTeamB=scoreTeamB+3;
-        displayForTeamB(scoreTeamB);
+        displayNumberTwo(scoreTeamB);
     }
     public void add2B(View view){
         scoreTeamB=scoreTeamB+2;
-        displayForTeamB(scoreTeamB);
+        displayNumberTwo(scoreTeamB);
     }
     public void freeB(View view){
         scoreTeamB=scoreTeamB+1;
-        displayForTeamB((scoreTeamB));
+        displayNumberTwo((scoreTeamB));
     }
     public void reset(View view){
         scoreTeamA=0;
         scoreTeamB=0;
-        displayForTeamB(scoreTeamB);
-        displayForTeamA(scoreTeamA);
+        displayNumberTwo(scoreTeamB);
+        displayNumberOne(scoreTeamA);
     }
 }
